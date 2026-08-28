c25d482b52c878103c35aff707c3898bdc876aae
espendalmo <espen.dalmo@me.com>
2026-08-28 20:46:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="gig-settliste-v7.7";
+const CACHE="gig-settliste-v7.8";
 const STATIC=["./","./index.html","./editor.html","./songs.json","./projects.json","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
